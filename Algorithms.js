adea621b229e6a0ec27f04755081f0c154556a45
Truk <hanqhoa01@gmail.com>
2023-06-14 11:29:04 +0700

    push test RR

--- Algorithms.js
+++ Algorithms.js
@@ -7,7 +7,7 @@ class Process {
       this.currentCpu = cpuBurstTimes.shift();
     }
 }
-
+let processes = [];
 function getData() {
     let processTable = document.getElementById('tableInput');
     let rows = processTable.getElementsByTagName('tr');
@@ -105,90 +105,116 @@ function pushReadyQueue(readyQueue,arrivalTime,processes){
   }
 }
 
-let processes = [];
+function checkCurrentCPU_toPush(process,currentProcess){
+  if(process.currentCpu == 0){
+    waitingQueues[currentProcess - 1] = process.ioTimes.shift();
+    process.currentCpu = process.cpuBurstTimes.shift();
+    tempProcess[currentProcess - 1] = process;
+  }else{
+    readyQueue.push(process);
+  }
+}
 
+function getProcessIndex(process){
+  let currentProcess;
+  for(let i = 1; i<=sumOfProcess; i++){
+    if(process.name == "P"+i){
+      currentProcess = i;
+      break;
+    }
+  }
+  return currentProcess;
+}
 
+function runIO(){
+  let row = Outputtable.rows;
+  for(let i = 0; i < sumOfProcess ; i++){
+    if(waitingQueues[i] != null){
+      waitingQueues[i]--;
+      row[i+1+sumOfProcess].cells[currentTime+1].textContent ="─────";
+      if(waitingQueues[i]== 0){
+        readyQueue.push(tempProcess[i]);
+        waitingQueues[i] = null;
+      }
+    }
+  }
+}
+
+function runCPU(cpuBursting,currentProcess,process){
+  let outputProcessRow = Outputtable.rows[currentProcess];
+  for(let i = 1; i <= cpuBursting; i++){
+    if(i == 1)
+      outputProcessRow.cells[currentTime+1].textContent = "|";
+    outputProcessRow.cells[currentTime+1].textContent +="─────";
+    process.currentCpu--;
+    //Kiểm tra tiến độ I/O của mỗi process qua mỗi lần Time tăng
+    runIO();
+    currentTime++;
+    //Check queue chưa vào
+    if(processes.length>0)
+      pushReadyQueue(readyQueue,currentTime,processesCopy);
+    printOutReadyQueue();
+  }
+  outputProcessRow.cells[currentTime].textContent += "|";
+}
+
+function checkCompletedProcess(process,currentProcess){
+  let outputProcessRow = Outputtable.rows[currentProcess];
+  if (process.cpuBurstTimes.length == 0 && process.currentCpu == 0) {
+    outputProcessRow.cells[currentTime+1].textContent="X";
+    return 1;
+  }
+  return 0;
+}
+
+function printOutReadyQueue(){
+  let rows = Outputtable.rows;
+  let readyQueueLength = readyQueue.length;
+  for(let i = 0; i < readyQueueLength; i++){
+    rows[i+1+sumOfProcess*2].cells[currentTime+1].textContent = readyQueue[i].name+readyQueue[i].currentCpu;
+  }
+}
+
+let Outputtable = document.getElementById("tableOutput");
+let readyQueue = [];
+let waitingQueues = [];
+let sumOfProcess; 
+let currentTime;
+let processesCopy; 
+let tempProcess = [];
 function roundRobin(timeQuantum) {
-  let table = document.getElementById("tableOutput");
-  let sumOfProcess = processes.length; 
-  let readyQueue = [];
-  let waitingQueues = [];
   //Khởi tạo ready queue
   let minArvTime = Math.min(...processes.map(process => process.arrivalTime));
-  pushReadyQueue(readyQueue,minArvTime,processes);
+  pushReadyQueue(readyQueue,minArvTime,processesCopy);
   //Khởi tạo thời gian hiện tại
-  let currentTime = minArvTime;
-  let tempProcess = [];
+  currentTime = minArvTime;
   //Thực hiện RR
   while (readyQueue.length > 0 ||  waitingQueues.some(Element => Element !== null)) {
+    printOutReadyQueue();
     if(readyQueue.length > 0){
+      printOutReadyQueue();
+      let rows = Outputtable.rows; 
+      rows[1+sumOfProcess*2].cells[currentTime+1].style.color ="red";
       let process = readyQueue.shift();
-      let currentProcess = null;
-        for(let i = 1; i<=sumOfProcess; i++){
-          if(process.name == "P"+i){
-            currentProcess = i;
-            break;
-          }
-        }
+      let currentProcess = getProcessIndex(process);
       // Thực thi CPU
-      let outputProcessRow = table.rows[currentProcess];
-      let outputIORow = table.rows;
       let cpuBursting = Math.min(timeQuantum,process.currentCpu);
-      for(let i = 1; i <= cpuBursting; i++){
-        if(i == 1)
-          outputProcessRow.cells[currentTime+1].textContent = "|";
-        outputProcessRow.cells[currentTime+1].textContent +="─────";
-        process.currentCpu--;
-        //Kiểm tra tiến độ I/O của mỗi process qua mỗi lần Time tăng
-        for(let i = 0; i < sumOfProcess ; i++){
-            if(waitingQueues[i] != null){
-              waitingQueues[i]--;
-              outputIORow[i+1+sumOfProcess].cells[currentTime+1].textContent ="─────";
-              if(waitingQueues[i]== 0){
-                readyQueue.push(tempProcess[i]);
-                waitingQueues[i] = null;
-              }
-            }
-        }
-        currentTime++;
-        //Check queue chưa vào
-        if(processes.length>0)
-          pushReadyQueue(readyQueue,currentTime,processes);
-      }
-      outputProcessRow.cells[currentTime].textContent += "|";
+      runCPU(cpuBursting,currentProcess,process);
 
       // Kiểm tra tiến trình đã hoàn thành hay chưa
-      if (process.cpuBurstTimes.length == 0 && process.currentCpu == 0) {
-        outputProcessRow.cells[currentTime+1].textContent="X";
+      if(checkCompletedProcess(process,currentProcess))
         continue;
-      }
-
       // Dưa I/O vào waiting queue
-      if(process.currentCpu == 0){
-        waitingQueues[currentProcess - 1] = process.ioTimes.shift();
-        process.currentCpu = process.cpuBurstTimes.shift();
-        tempProcess[currentProcess - 1] = process;
-      }else{
-        readyQueue.push(process);
-      }
+      checkCurrentCPU_toPush(process,currentProcess);
     }else{
       //thực thi I/O khi readyQueue trống
-      let outputIORow = table.rows;
       if(readyQueue.length == 0){
-        for(let i = 0; i < sumOfProcess ; i++){
-          if(waitingQueues[i] != null){
-            waitingQueues[i]--;
-            outputIORow[i+1+sumOfProcess].cells[currentTime+1].textContent ="─────";
-            if(waitingQueues[i]==0){
-              readyQueue.push(tempProcess[i]);
-              waitingQueues[i] = null;
-            }
-          }
-        }
+        runIO();
         currentTime++;
         //Check queue chưa vào
         if(processes.length>0)
-          pushReadyQueue(readyQueue,currentTime,processes);
+          pushReadyQueue(readyQueue,currentTime,processesCopy);
+        printOutReadyQueue();
       }
     }
   }
@@ -196,6 +222,8 @@ function roundRobin(timeQuantum) {
 let solveBtn = document.getElementById('solve');
 solveBtn.addEventListener('click', function() {
   getData();
+  sumOfProcess = processes.length; 
+  processesCopy = [...processes];
   setOutputForm(20);
   roundRobin(2);
 });
